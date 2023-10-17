2744838f64f7d9f0c03fa335b5f6ebbeec887348
Dan J Bower <djbower@users.noreply.github.com>
2023-10-17 10:14:03 +0200

    Improve logging and mesh generation

--- spider/__init__.py
+++ spider/__init__.py
@@ -1,4 +1,7 @@
-"""Package level variables and initialises the package logger."""
+"""Package level variables and initialises the package logger.
+
+See the LICENSE file for licensing information.
+"""
 
 __version__: str = "0.1.0"
 
@@ -7,34 +10,61 @@ import logging
 
 from thermochem import codata
 
-# Create the package logger.
-logger: logging.Logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-# Create a handler for the logger.
-handler: logging.Handler = logging.StreamHandler()
-handler.setLevel(logging.DEBUG)
-
-# Create a formatter for the log messages.
-# Simple formatter.
-fmt: str = "%(asctime)s - %(name)-30s - %(levelname)-9s - %(message)s"
-datefmt: str = "%H:%M:%S"
-
-# Complex formatter.
-# fmt: str = "[%(asctime)s - %(name)-20s - %(lineno)03d - %(levelname)-9s -
-# %(funcName)s()] %(message)s"
-# datefmt: str = "Y-%m-%d %H:%M:%S"
-formatter: logging.Formatter = logging.Formatter(fmt, datefmt=datefmt)
-handler.setFormatter(formatter)
-
-# Add the handler to the logger.
-logger.addHandler(handler)
-
-logger.info("%s version %s", __name__, __version__)
-
 # Module constants.
 GAS_CONSTANT: float = codata.value("molar gas constant")  # J/K/mol.
 GRAVITATIONAL_CONSTANT: float = codata.value("Newtonian constant of gravitation")  # m^3/kg/s^2.
 OCEAN_MOLES: float = 7.68894973907177e22  # Moles of H2 (or H2O) in one present-day Earth ocean.
 
-DATA_ROOT_PATH = importlib.resources.files("spider.data")
+DATA_ROOT_PATH = importlib.resources.files("%s.data" % __package__)
+
+# Create the package logger.
+# https://docs.python.org/3/howto/logging.html#library-config
+logger: logging.Logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+
+def complex_formatter() -> logging.Formatter:
+    """Complex formatter."""
+    fmt: str = "[%(asctime)s - %(name)-30s - %(lineno)03d - %(levelname)-9s - %(funcName)s()] - %(message)s"
+    datefmt: str = "%Y-%m-%d %H:%M:%S"
+    formatter: logging.Formatter = logging.Formatter(fmt, datefmt=datefmt)
+    return formatter
+
+
+def simple_formatter() -> logging.Formatter:
+    """Simple formatter."""
+    fmt: str = "[%(asctime)s - %(name)-30s - %(levelname)-9s] - %(message)s"
+    datefmt: str = "%H:%M:%S"
+    formatter: logging.Formatter = logging.Formatter(fmt, datefmt=datefmt)
+    return formatter
+
+
+def debug_logger() -> None:
+    """Setup the logging for debugging: DEBUG to the console."""
+    # Console logger.
+    logger: logging.Logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    logger.handlers = []
+    console_handler: logging.Handler = logging.StreamHandler()
+    console_formatter: logging.Formatter = simple_formatter()
+    console_handler.setFormatter(console_formatter)
+    logger.addHandler(console_handler)
+
+
+def debug_file_logger() -> None:
+    """Setup the logging to a file (DEBUG) and to the console (INFO)."""
+    # Console logger.
+    logger: logging.Logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+    logger.handlers = []
+    console_handler: logging.Handler = logging.StreamHandler()
+    console_formatter: logging.Formatter = simple_formatter()
+    console_handler.setFormatter(console_formatter)
+    console_handler.setLevel(logging.INFO)
+    logger.addHandler(console_handler)
+    # File logger.
+    file_handler: logging.Handler = logging.FileHandler("%s.log" % __package__)
+    file_formatter: logging.Formatter = complex_formatter()
+    file_handler.setFormatter(file_formatter)
+    file_handler.setLevel(logging.DEBUG)
+    logger.addHandler(file_handler)
