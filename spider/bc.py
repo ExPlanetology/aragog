cfd0120b2f30166f756d6df66ed960acd6d126d3
Dan J Bower <djbower@users.noreply.github.com>
2023-11-13 16:24:46 +0100

    Update bc.py

--- spider/bc.py
+++ spider/bc.py
@@ -25,7 +25,16 @@ class BoundaryConditions(DataclassFromConfiguration):
     """Boundary conditions
 
     Args:
-        # TODO
+        scalings: Scalings
+        outer_boundary_condition: TODO
+        outer_boundary_value: TODO
+        inner_boundary_condition: TODO
+        inner_boundary_value: TODO
+        emissivity: TODO
+        equilibrium_temperature: TODO
+        core_radius: TODO
+        core_density: TODO
+        core_heat_capacity: TODO
 
     Attributes:
         # TODO
@@ -43,8 +52,12 @@ class BoundaryConditions(DataclassFromConfiguration):
     core_density: float
     core_heat_capacity: float
 
-    def _grey_body(self, state: State) -> None:
-        """Applies a grey body at the surface."""
+    def grey_body(self, state: State) -> None:
+        """Applies a grey body flux at the surface.
+
+        Args:
+            state: The state to apply the boundary conditions to
+        """
         state.heat_flux[-1, :] = (
             self.emissivity
             * self.scalings.stefan_boltzmann_constant
@@ -55,11 +68,19 @@ class BoundaryConditions(DataclassFromConfiguration):
         )
 
     def apply(self, state: State) -> None:
-        """Applies the boundary conditions."""
+        """Applies the boundary conditions to the state.
+
+        Args:
+            state: The state to apply the boundary conditions to
+        """
         self.core_heat_flux(state)
-        self._grey_body(state)
+        self.grey_body(state)
 
     def core_heat_flux(self, state: State) -> None:
-        """Applies the heat flux at the core-mantle boundary."""
+        """Applies the heat flux at the core-mantle boundary.
+
+        Args:
+            state: The state to apply the boundary conditions to
+        """
         # No heat flux from the core.
         state.heat_flux[0, :] = 0
