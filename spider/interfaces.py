58d0aec9dae802fe96391ea63f19783910a92e2a
Dan J Bower <djbower@users.noreply.github.com>
2023-11-30 11:32:08 +0100

    Move class to interfaces

--- spider/interfaces.py
+++ spider/interfaces.py
@@ -7,7 +7,7 @@ from __future__ import annotations
 
 import inspect
 import logging
-from abc import abstractmethod
+from abc import ABC, abstractmethod
 from configparser import SectionProxy
 from dataclasses import dataclass, field
 from typing import Any, Self
@@ -129,3 +129,32 @@ class Scalings(ScaledDataclassFromConfiguration):
             self.power_per_volume * self.radius / np.power(self.temperature, 4)
         )
         logger.debug("scalings = %s", self)
+
+
+@dataclass(kw_only=True, frozen=True)
+class PropertyABC(ABC):
+    """A property whose value can be evaluated at temperature and pressure.
+
+    Args:
+        name: Name of the property
+
+    Attributes:
+        name: Name of the property
+    """
+
+    name: str
+
+    @abstractmethod
+    def get_value(self, temperature: np.ndarray, pressure: np.ndarray) -> np.ndarray:
+        """Computes the property value at temperature and pressure.
+
+        Args:
+            temperature: Temperature
+            pressure: Pressure
+
+        Returns:
+            The property value evaluated at temperature and pressure.
+        """
+
+    def __call__(self, temperature: np.ndarray, pressure: np.ndarray) -> np.ndarray:
+        return self.get_value(temperature, pressure)
