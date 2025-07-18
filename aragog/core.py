339e17a85e5c7d2f49cbb37a3c480ed9a47e82a2
Laurent Soucasse <135331272+lsoucasse@users.noreply.github.com>
2025-07-18 10:36:24 +0200

    Rename delta radii into delta mesh.

--- aragog/core.py
+++ aragog/core.py
@@ -69,7 +69,7 @@ class BoundaryConditions:
         if self._settings.inner_boundary_condition == 3:
             temperature_basic[0, :] = self._settings.inner_boundary_value
             dTdr[0, :] = (
-                2 * (temperature[0, :] - temperature_basic[0, :]) / self._mesh.basic.delta_radii[0]
+                2 * (temperature[0, :] - temperature_basic[0, :]) / self._mesh.basic.delta_mesh[0]
             )
         # Surface
         if self._settings.outer_boundary_condition == 5:
@@ -77,7 +77,7 @@ class BoundaryConditions:
             dTdr[-1, :] = (
                 2
                 * (temperature_basic[-1, :] - temperature[-1, :])
-                / self._mesh.basic.delta_radii[-1]
+                / self._mesh.basic.delta_mesh[-1]
             )
 
     def apply_temperature_boundary_conditions_melt(
@@ -95,14 +95,14 @@ class BoundaryConditions:
         if self._settings.inner_boundary_condition == 3:
             dphidr[0, :] = (
                 2 * (melt_fraction[0, :] - melt_fraction_basic[0, :])
-                / self._mesh.basic.delta_radii[0]
+                / self._mesh.basic.delta_mesh[0]
             )
         # Surface
         if self._settings.outer_boundary_condition == 5:
             dphidr[-1, :] = (
                 2
                 * (melt_fraction_basic[-1, :] - melt_fraction[-1, :])
-                / self._mesh.basic.delta_radii[-1]
+                / self._mesh.basic.delta_mesh[-1]
             )
 
     def apply_flux_boundary_conditions(self, state: State) -> None:
